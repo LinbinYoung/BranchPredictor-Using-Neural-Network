dd29f9a3d5c80bd0b4ed4f665311b0a1f634bc2c
LinbinYoung <sheeplinbean@163.com>
2019-05-31 18:31:22 -0700

    First Commit

---
+++ src/custom.c
@@ -0,0 +1,168 @@
+#include <stdio.h>
+
+uint32_t Global_History_custom;
+uint8_t* PHT_tor_custom;
+uint32_t PHT_SIZE_custom;
+uint32_t gmask_tor_custom;
+
+uint32_t* LHT_custom;
+uint32_t LHT_SIZE_custom;
+uint32_t pcmask_custom;
+uint32_t lmask_custom;
+
+uint8_t* CPT_custom;
+int32_t** WEIGHT;
+
+void initialized_CUSTOM(){
+    // Compared with tournament, the network methods adopt
+    Global_History_custom = 0;
+    PHT_SIZE_custom = 1 << ghistoryBits;
+    gmask_tor_custom = PHT_SIZE_custom - 1;
+    lmask_custom = (1 << lhistoryBits) - 1;
+    PHT_tor_custom = (uint8_t*)malloc(sizeof(uint8_t)*PHT_SIZE_custom);
+    for (int i = 0; i < PHT_SIZE_custom; i++){
+        PHT_tor_custom[i] = 1;  
+    }
+
+    LHT_SIZE_custom = 1 << pcIndexBits;
+    pcmask_custom = LHT_SIZE_custom - 1;
+    LHT_custom = (uint32_t*)malloc(sizeof(uint32_t)*LHT_SIZE_custom);
+    for (int i = 0; i < LHT_SIZE_custom; i++){
+        LHT_custom[i] = 0;
+    }
+
+    CPT_custom = (uint8_t*)malloc(sizeof(uint8_t)*PHT_SIZE_custom);
+    for (int i = 0; i < PHT_SIZE_custom; i++){
+        CPT_custom[i] = 2;
+    }
+
+    //intialized the two-dimension weight array
+    WEIGHT = (int32_t**)malloc(sizeof(int32_t*)*LHT_SIZE_custom);
+    for (int i = 0; i < LHT_SIZE_custom; i ++){
+        WEIGHT[i] = (int32_t*)malloc(sizeof(int32_t)*(lhistoryBits + 1));
+    }//end for
+}
+
+uint8_t Choice_maker_custom(){
+    uint32_t index_c = Global_History_custom & gmask_tor_custom;
+    uint8_t res_choice = CPT_custom[index_c];
+    if (res_choice < 2){
+        // 0, 1
+        return 0;
+    }else{
+        // 2, 3
+        return 1;
+    }
+}
+
+uint8_t Get_Global_Res_custom(){
+    uint32_t index = Global_History_custom & gmask_tor_custom;
+    uint8_t global_res = PHT_tor_custom[index];
+    return global_res;
+}
+
+int32_t Perceptron_predict(uint32_t pc){
+    uint32_t index_c = pc & pcmask_custom;
+    int32_t* weight_for_branch = WEIGHT[index_c];
+    uint32_t history_record = LHT_custom[index_c] & lmask_custom;
+    int32_t sum = weight_for_branch[0];
+    for (int i = 0; i < lhistoryBits; i ++){
+        int32_t res = (history_record >> i) & 1;
+        if (res == 0){
+            res = -1;
+        }
+        sum = sum + weight_for_branch[i+1] * res;
+    }
+    return sum;
+}
+
+uint8_t CUSTOM_predictor(uint32_t pc){
+    uint8_t global_res = Get_Global_Res_custom();
+    uint8_t our_choice = Choice_maker_custom();
+    int32_t sum = Perceptron_predict(pc);
+    if (our_choice == 0){
+        if (global_res > 1){
+            return TAKEN;
+        }else{
+            return NOTTAKEN;
+        }
+    }else{
+        if (sum > 0){
+            return TAKEN;
+        }else{
+            return NOTTAKEN;
+        }
+    }
+}
+
+void train_CUSTOM(uint32_t pc, uint8_t outcome){
+    //get all the necessary indexs and predictions before any modification
+    uint32_t index = Global_History_custom & gmask_tor_custom;
+    uint8_t global_res = PHT_tor_custom[index];
+    
+    uint32_t index_c = pc & pcmask_custom;
+    int32_t* weight_for_branch = WEIGHT[index_c];
+    uint32_t history_record = LHT_custom[index_c] & lmask_custom;
+    int32_t sum = weight_for_branch[0];
+    for (int i = 0; i < lhistoryBits; i ++){
+        int32_t res = (history_record >> i) & 1;
+        if (res == 0){
+            res = -1;
+        }
+        sum = sum + weight_for_branch[i+1] * res;
+    }
+
+    uint8_t lres;
+    uint8_t gres;
+    if (global_res > 1){
+        gres = TAKEN;
+    }else{
+        gres = NOTTAKEN;
+    }
+    if (sum > 0){
+        lres = TAKEN;
+    }else{
+        lres = NOTTAKEN;
+    }
+    //update wieight
+    int32_t actual_outcome = 1;
+    if (outcome == 0){
+        actual_outcome = -1;
+    }
+    if (outcome != lres){
+        weight_for_branch[0] = weight_for_branch[0] + actual_outcome*1;
+        for (int i = 0; i < lhistoryBits; i ++){
+            int32_t his = 1;
+            uint32_t record = (history_record >> i) & 1;
+            if (record == 0){
+                his = -1;
+            }
+            weight_for_branch[i+1] = weight_for_branch[i+1] + actual_outcome*his;
+        }
+        WEIGHT[index_c] = weight_for_branch;
+    }
+    //update CPT
+    if (lres != outcome && gres == outcome){
+        if (CPT_custom[index] != 0){
+            CPT_custom[index] = CPT_custom[index] - 1;
+        }
+    }else if (lres == outcome && gres != outcome){
+        if (CPT_custom[index] != 3){
+            CPT_custom[index] = CPT_custom[index] + 1;
+        }
+    }
+    //update PHT_or
+    if (outcome == TAKEN){
+        if (PHT_tor_custom[index] != 3){
+            PHT_tor_custom[index] = PHT_tor_custom[index] + 1;
+        }
+    }else{
+        if (PHT_tor_custom[index] != 0){
+            PHT_tor_custom[index] = PHT_tor_custom[index] - 1;
+        }
+    }
+    // update record
+    history_record = (history_record << 1 | outcome) & lmask_custom;
+    LHT_custom[index_c] = history_record;
+    Global_History_custom = (Global_History_custom << 1 | outcome) & gmask_tor_custom;
+}
---
+++ src/predictor.c
@@ -0,0 +1,304 @@
+//========================================================//
+//  predictor.c                                           //
+//  Source file for the Branch Predictor                  //
+//                                                        //
+//  Implement the various branch predictors below as      //
+//  described in the README                               //
+//========================================================//
+#include <stdio.h>
+#include "predictor.h"
+
+//
+// TODO:Student Information
+//
+const char *studentName = "Shuai Hao";
+const char *studentID   = "A53274051";
+const char *email       = "s1hao@eng.ucsd.edu";
+
+//------------------------------------//
+//      Predictor Configuration       //
+//------------------------------------//
+
+// Handy Global for use in output routines
+const char *bpName[4] = { "Static", "Gshare",
+                          "Tournament", "Custom" };
+
+int ghistoryBits; // Number of bits used for Global History
+int lhistoryBits; // Number of bits used for Local History
+int pcIndexBits;  // Number of bits used for PC index
+int bpType;       // Branch Prediction Type
+int verbose;
+
+#include "custom.c"
+//------------------------------------//
+//      Predictor Data Structures     //
+//------------------------------------//
+
+//
+//TODO: Add your own Branch Predictor data structures here
+//
+
+
+uint32_t Global_History_gshare;
+uint8_t* PHT_gshare;
+uint32_t PHT_SIZE_gshare;
+uint32_t gmask_gshare;
+
+uint32_t Global_History;
+uint8_t* PHT_tor;
+uint32_t PHT_SIZE;
+uint32_t gmask_tor;
+
+uint32_t* LHT;
+uint32_t LHT_SIZE;
+uint32_t pcmask;
+
+uint8_t* LPT;
+uint32_t LPT_SIZE;
+uint32_t lmask;
+
+uint8_t* CPT;
+
+
+//------------------------------------//
+//        Predictor Functions         //
+//------------------------------------//
+
+// Initialize the predictor
+
+void initialized_STATIC(){
+
+}
+
+void initialized_GSHARE(){
+    Global_History_gshare = 0;
+    PHT_SIZE_gshare = 1 << ghistoryBits;
+    gmask_gshare = PHT_SIZE_gshare - 1;
+    PHT_gshare = (uint8_t*)malloc(sizeof(uint8_t)*PHT_SIZE_gshare);
+    for (int i = 0; i < PHT_SIZE_gshare; i++){
+        PHT_gshare[i] = 1;
+    }
+}
+
+void initialized_TOURNAMENT(){
+    Global_History = 0;
+    PHT_SIZE = 1 << ghistoryBits;
+    gmask_tor = PHT_SIZE - 1;
+    PHT_tor = (uint8_t*)malloc(sizeof(uint8_t)*PHT_SIZE);
+    for (int i = 0; i < PHT_SIZE; i++){
+        PHT_tor[i] = 1;  
+    }
+  
+    LHT_SIZE = 1 << pcIndexBits;
+    pcmask = LHT_SIZE - 1;
+    LHT = (uint32_t*)malloc(sizeof(uint32_t)*LHT_SIZE);
+    for (int i = 0; i < LHT_SIZE; i++){
+        LHT[i] = 0;
+    }
+  
+    LPT_SIZE = 1 << lhistoryBits;
+    lmask = LPT_SIZE - 1;
+    LPT = (uint8_t*)malloc(sizeof(uint8_t)*LPT_SIZE);
+    for (int i = 0; i < LPT_SIZE; i++){
+        LPT[i] = 1;
+    }
+  
+    CPT = (uint8_t*)malloc(sizeof(uint8_t)*PHT_SIZE);
+    for (int i = 0; i < PHT_SIZE; i++){
+        CPT[i] = 2;
+    }
+}
+
+
+void init_predictor()
+{
+  switch (bpType){
+    case STATIC:
+      initialized_STATIC();
+    case GSHARE:
+      initialized_GSHARE();
+    case TOURNAMENT:
+      initialized_CUSTOM();
+    case CUSTOM:
+    //   initialized_CUSTOM();
+    default:
+      break;
+  }
+  return;
+}
+
+// Make a prediction for conditional branch instruction at PC 'pc'
+// Returning TAKEN indicates a prediction of taken; returning NOTTAKEN
+// indicates a prediction of not taken
+//
+
+uint8_t GSHARE_predictor(uint32_t pc){
+    uint32_t index = (pc ^ Global_History_gshare) & gmask_gshare;
+    uint8_t res = PHT_gshare[index];
+    if (res > 1){
+        return TAKEN;
+    }else{
+        return NOTTAKEN;
+    }
+}
+
+uint8_t Choice_maker(){
+    uint32_t index_c = Global_History & gmask_tor;
+    uint8_t res_choice = CPT[index_c];
+    if (res_choice < 2){
+        // 0, 1
+        return 0;
+    }else{
+        // 2, 3
+        return 1;
+    }
+}
+
+uint8_t Get_Local_Res(uint32_t pc){
+    uint32_t histroy_index = pc & pcmask;
+    uint32_t history_record = LHT[histroy_index] & lmask;
+    uint8_t local_res = LPT[history_record];
+    return local_res;
+}
+
+uint8_t Get_Global_Res(){
+    uint32_t index = Global_History & gmask_tor;
+    uint8_t global_res = PHT_tor[index];
+    return global_res;
+}
+
+uint8_t TOURNAMENT_predictor(uint32_t pc){
+    uint8_t local_res = Get_Local_Res(pc);
+    uint8_t global_res = Get_Global_Res();
+    uint8_t our_choice = Choice_maker();
+    if (our_choice == 0){
+        if (global_res > 1){
+            return TAKEN;             
+        }else{
+            return NOTTAKEN;
+        }
+    }else{
+        if (local_res > 1){
+            return TAKEN;
+        }else{
+            return NOTTAKEN;
+        }
+    }
+}
+
+uint8_t make_prediction(uint32_t pc)
+{
+  //
+  //TODO: Implement prediction scheme
+  //
+  // Make a prediction based on the bpType
+  switch (bpType) {
+    case STATIC:
+      return TAKEN;
+    case GSHARE:
+      return GSHARE_predictor(pc);
+    case TOURNAMENT:
+      return CUSTOM_predictor(pc);
+    case CUSTOM:
+    //   return CUSTOM_predictor(pc);
+    default:
+      break;
+  }
+
+  // If there is not a compatable bpType then return NOTTAKEN
+  return NOTTAKEN;
+}
+
+// Train the predictor the last executed branch at PC 'pc' and with
+// outcome 'outcome' (true indicates that the branch was taken, false
+// indicates that the branch was not taken)
+//
+
+void train_STATIC(uint32_t pc, uint8_t outcome){
+
+}
+
+void train_GSHARE(uint32_t pc, uint8_t outcome){
+    uint32_t index = (pc ^ Global_History_gshare) & gmask_gshare;
+    if (outcome == TAKEN){
+        if (PHT_gshare[index] != 3){
+            PHT_gshare[index] = PHT_gshare[index] + 1;
+        }  
+    }else{
+        if (PHT_gshare[index] != 0){
+            PHT_gshare[index] = PHT_gshare[index] - 1;
+        }
+    }
+    Global_History_gshare = (Global_History_gshare << 1 | outcome) & gmask_gshare;
+}
+
+
+void train_TOURNAMENT(uint32_t pc, uint8_t outcome){
+    uint32_t histroy_index = pc & pcmask;
+    uint32_t history_record = LHT[histroy_index] & lmask;
+    uint8_t local_res = LPT[history_record];
+    uint32_t index = Global_History & gmask_tor;
+    uint8_t global_res = PHT_tor[index];
+    uint8_t lpre = 0;
+    uint8_t gpre = 0;
+    if (global_res > 1){
+        gpre = TAKEN;
+    }else{
+        gpre = NOTTAKEN;
+    }
+    if (local_res > 1){
+        lpre = TAKEN;
+    }else{
+        lpre = NOTTAKEN;
+    }
+    if (outcome == TAKEN){
+        if (LPT[history_record] != 3){
+            LPT[history_record] = LPT[history_record] + 1;
+        }
+    }else{
+        if (LPT[history_record] != 0){
+            LPT[history_record] = LPT[history_record] - 1;
+        }
+    }
+    if (gpre == outcome && lpre != outcome){
+        if (CPT[index] != 0){
+            CPT[index] = CPT[index] - 1;
+        }
+    }else if(gpre != outcome && lpre == outcome){
+        if (CPT[index] != 3){
+            CPT[index] = CPT[index] + 1;
+        }
+    }
+    if (outcome == TAKEN){
+        if (PHT_tor[index] != 3){
+            PHT_tor[index] = PHT_tor[index] + 1;
+        }
+    }else{
+        if (PHT_tor[index] != 0){
+            PHT_tor[index] = PHT_tor[index] - 1;
+        }
+    }
+    //update history record
+    history_record = (history_record << 1 | outcome) & lmask;
+    LHT[histroy_index] = history_record;
+    Global_History = (Global_History << 1 | outcome) & gmask_tor;
+}
+
+
+
+void train_predictor(uint32_t pc, uint8_t outcome)
+{
+  switch (bpType){
+    case STATIC:
+      train_STATIC(pc, outcome);
+    case GSHARE:
+      train_GSHARE(pc, outcome);
+    case TOURNAMENT:
+      train_CUSTOM(pc, outcome);
+    case CUSTOM:
+    //   train_CUSTOM(pc, outcome);
+    default:
+      break;
+  }
+  return;
+}
